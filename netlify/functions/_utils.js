e3dde888625ddfda4e11ae176d80ca8f15310c93
Muhammad Muneer <admin@fexer.it.com>
2026-07-10 02:37:48 +0500

    web update

--- netlify/functions/_utils.js
+++
@@ -1,8 +0,0 @@
-function resp(statusCode, body) {
-    return {
-        statusCode,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body)
-    };
-}
-module.exports = { resp };
